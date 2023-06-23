317906ee24a50a9d3f34cb691c572337ffdb23fa
Sergio Lopez <sergio0695@gmail.com>
2023-06-22 23:52:35 -0600

    test

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -8,6 +8,34 @@ class ChatController extends Controller
 {
     //
     static public function getResponse($data) {
-        return $data["message"];
+        $category = ChatController::detectarCategoria($data["message"]);
+
+        switch ($category) {
+            case 'saludo':
+                return "estas saludando";
+            case 'información':
+                return "estas pidiendo información";
+            default:
+                return $data['message'];
+        }
+    }
+
+    static private function detectarCategoria($texto)
+    {
+
+        $categorias = [
+            'saludo' => ['buenos dias','buenas tardes','buenas noches'],
+            'información' => ['informacion', 'información', 'disponibles','disponible'],
+        ];
+
+        foreach ($categorias as $categoria => $palabrasClave) {
+            foreach ($palabrasClave as $palabra) {
+                if (stripos($texto, $palabra) !== false) {
+                    return $categoria;
+                }
+            }
+        }
+
+        return 'Categoría no detectada';
     }
 }
