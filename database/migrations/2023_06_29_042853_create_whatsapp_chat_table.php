33c2d87dd7f40fd123430b60dc5ec50b87881b86
Sergio Lopez <sergio0695@gmail.com>
2023-06-30 00:35:46 -0600

    fix migrations

--- database/migrations/2023_06_29_042853_create_whatsapp_chat_table.php
+++ database/migrations/2023_06_29_042853_create_whatsapp_chat_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('whatsapp_chat', function (Blueprint $table) {
+        Schema::create('whatsapp_chats', function (Blueprint $table) {
             $table->id();
             $table->string('phoneID');
             $table->unsignedBigInteger('whatsapp_question_id')->nullable();
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('whatsapp_chat');
+        Schema::dropIfExists('whatsapp_chats');
     }
 };
