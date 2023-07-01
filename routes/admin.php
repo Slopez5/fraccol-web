b021a6b0dacead26ef2f06aa39b0382ceccd9a2e
Sergio Lopez <sergio0695@gmail.com>
2023-07-01 00:23:22 -0600

    api clear all chats

--- routes/admin.php
+++ routes/admin.php
@@ -4,10 +4,14 @@ use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\admin\DevelopmentController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\EnterpriseController;
+use App\Models\WhatsappChat;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [DashboardController::class,'dashboard']);
 Route::get('/development/{id}', [DevelopmentController::class,'details'])->name('development.details');
 Route::get('/images', [DevelopmentController::class,'images'])->name('development.images');
 Route::get('/chatbot/questions/create', [ChatController::class,'create'])->name('chatbot.questions.create');
-Route::POST('/chatbot/questions/store', [ChatController::class,'store'])->name('chatbot.questions.store');
+Route::post('/chatbot/questions/store', [ChatController::class,'store'])->name('chatbot.questions.store');
+Route::post('chatbot/chats', function () {
+    WhatsappChat::truncate();
+});
