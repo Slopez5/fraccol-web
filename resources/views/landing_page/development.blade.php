c17442aa6c6282932445f0ce5667b7fc2401f4e8
Sergio Lopez <sergio0695@gmail.com>
2023-07-23 14:14:27 -0600

    Rediseno de landing page

--- resources/views/landing_page/development.blade.php
+++ resources/views/landing_page/development.blade.php
@@ -1,74 +1,208 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fraccionamiento Campestre</title>
-    <link rel="stylesheet" href="{{ asset('css/landingPage/development.css') }}">
+    <title>{{ $development->name }} - Tu hogar en la naturaleza</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .price li{
+            display: block;
+        }
+    </style>
 </head>
+
 <body>
-    <!-- Encabezado -->
-    <header>
-        <h1>Fraccionamiento {{ $development->name }}</h1>
-        <nav>
-            <ul>
-                <li><a href="#inicio">Inicio</a></li>
-                <li><a href="#ubicacion">Ubicación</a></li>
-                <li><a href="#desarrollo">Desarrollo</a></li>
-                <li><a href="#contacto">Contacto</a></li>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">{{ $development->name }} </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#features">Características</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#gallery">Galería</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#prices">Precios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#plan">Plano de Distribución</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contacto</a>
+                </li>
             </ul>
-        </nav>
-    </header>
-
-    <!-- Sección Inicio -->
-    <section id="inicio">
-        <div class="banner">
-            <img src="fraccionamiento.jpg" alt="Fraccionamiento Campestre">
         </div>
-        <div class="contenido">
-            <h2>Fraccionamiento {{ $development->name }}</h2>
-            <p>Un lugar rodeado de naturaleza y tranquilidad, donde podrás disfrutar de momentos inolvidables junto a tu familia.</p>
-            <a href="#contacto" class="boton">Contáctanos</a>
+    </nav>
+
+    <!-- Hero Section -->
+    <section class="hero-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h1>Bienvenido {{ $development->name }} </h1>
+                    <p>Descubre tu hogar en medio de la naturaleza</p>
+                    <a href="#contact" class="btn btn-primary">Contáctanos</a>
+                </div>
+                <div class="col-lg-6">
+                    <img src="https://scontent.fgdl5-3.fna.fbcdn.net/v/t39.30808-6/326075293_1223707178259593_8891253910133581331_n.jpg?_nc_cat=100&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeE9v-pmFxp-dNi5mJIhPbkAnoksQJSUOpOeiSxAlJQ6kxTzDr7KlFGBQB9W4xa39BCywwvRknpRvhJBCLv9gfO0&_nc_ohc=m3EJEhdhom8AX9Jhk3O&_nc_ht=scontent.fgdl5-3.fna&oh=00_AfBr9pnlJ4TtJTEiozbjWkoAN5_bo5RokHUO3zZNIdO9MQ&oe=64BB0A67"
+                        alt="Fraccionamiento Campestre" class="img-fluid">
+                </div>
+            </div>
         </div>
     </section>
 
-    <!-- Sección Ubicación -->
-    <section id="ubicacion">
-        <h2>Ubicación</h2>
-        <p>Nuestro fraccionamiento campestre se encuentra en una zona privilegiada, rodeado de hermosos paisajes y con fácil acceso desde la ciudad.</p>
-        <!-- Agregar mapa interactivo con la ubicación -->
+    <!-- Features Section -->
+    <section id="features" class="features-section">
+        <div class="container">
+            <h2 class="text-center">Características del Fraccionamiento</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="feature-item">
+                        <i class="fas fa-tree"></i>
+                        <h3>Entorno Natural</h3>
+                        <p>Disfruta de un entorno natural rodeado de árboles y áreas verdes.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="feature-item">
+                        <i class="fas fa-swimming-pool"></i>
+                        <h3>GYM al aire libre</h3>
+                        <p>Relájate y disfruta del gym al aire libre</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="feature-item">
+                        <i class="fas fa-child"></i>
+                        <h3>Áreas de Juego</h3>
+                        <p>Diviértete con tus hijos en nuestras áreas de juego seguras y divertidas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <!-- Sección Desarrollo -->
-    <section id="desarrollo">
-        <h2>Desarrollo</h2>
-        <p>Nuestro fraccionamiento está proyectado a 5 años y contará con diversas amenidades y servicios para que disfrutes de la vida en la naturaleza:</p>
-        <ul>
-            <li>Pistas de senderismo</li>
-            <li>Áreas verdes y parques</li>
-            <li>Piscinas y zonas recreativas</li>
-            <li>Club social</li>
-            <li>Seguridad las 24 horas</li>
-        </ul>
-        <a href="#contacto" class="boton">Solicita más información</a>
+    <!-- Gallery Section -->
+    <section id="gallery" class="gallery-section">
+        <div class="container">
+            <h2 class="text-center">Galería de Imágenes</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="gallery-item">
+                        <img src="https://scontent.fgdl5-1.fna.fbcdn.net/v/t39.30808-6/292779576_499849785274617_3562468866102891604_n.jpg?_nc_cat=103&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFfj6ZOJyC83S0xCP4J_W51O8OpwErYUnk7w6nASthSef6ujay3WlaP3gRH3mRseEGYpI3myAaFBt1qq-dKQUdV&_nc_ohc=cHQxMhIKfQQAX_igla4&_nc_ht=scontent.fgdl5-1.fna&oh=00_AfBmD4SFudrxN1uyAviI9ohM26s2bZw3fp5yeL84baThfQ&oe=64BAB3C5"
+                            alt="Galería 1" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="gallery-item">
+                        <img src="https://scontent.fgdl5-3.fna.fbcdn.net/v/t39.30808-6/293129925_499849838607945_291786880913172582_n.jpg?_nc_cat=107&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeGG1KJEqgFvzGoQ5rlJsMXlEaQOKhkUxh0RpA4qGRTGHV6BbwzPhreKn169uB0IFFADDVS64qPzPPi3kYYkLUH8&_nc_ohc=HFoYgpEb6BkAX8g1lwq&_nc_ht=scontent.fgdl5-3.fna&oh=00_AfCArCOELFz5-skyWbnC7utCL7KW_5Ty7DCmS3Z8T607rA&oe=64BA8B63"
+                            alt="Galería 2" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="gallery-item">
+                        <img src="https://scontent.fgdl5-1.fna.fbcdn.net/v/t39.30808-6/293315709_499849878607941_2629861164433938007_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFfKuk8fF64XEbhaUR_lxAkjaCfQD4qb5eNoJ9APipvly8omkggZQ6OT65r1wkTsiyqvMx7OKSNqsQEpDxLjRsB&_nc_ohc=uiw92x1RX7sAX-y_aw_&_nc_ht=scontent.fgdl5-1.fna&oh=00_AfDiiu38g52c9PG0yibE1O7fv1tlyjxhyqwu0yTqiz9wzw&oe=64BA7975"
+                            alt="Galería 3" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <!-- Sección Contacto -->
-    <section id="contacto">
-        <h2>Contacto</h2>
-        <p>Si estás interesado en conocer más sobre nuestro fraccionamiento, déjanos tus datos y te contactaremos a la brevedad.</p>
-        <form action="#" method="post">
-            <input type="text" name="nombre" placeholder="Nombre" required>
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="tel" name="telefono" placeholder="Teléfono" required>
-            <textarea name="mensaje" placeholder="Mensaje" required></textarea>
-            <button type="submit" class="boton">Enviar mensaje</button>
-        </form>
+    <!-- Prices Section -->
+    <section id="prices" class="prices-section">
+        <div class="container">
+            <h2 class="text-center">Precios</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="price-item">
+                        <h3>Contado</h3>
+                        <p>Precio: $1,150m<sup>2</sup></p>
+                    </div>
+                </div>
+
+                <div class="col-md-5">
+                    <div class="price-item text-center">
+                        <h3>Financiamiento</h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="price">
+                                    <li>1 año: $1,199m<sup>2</sup></li>
+                                    <li>2 año: $1,248m<sup>2</sup></li>
+                                    <li>3 año: $1,297m<sup>2</sup></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="price">
+                                    <li>4 año: $1,346m<sup>2</sup></li>
+                                    <li>5 año: $1,395m<sup>2</sup></li>
+                                    <li>6 año: $1,444m<sup>2</sup></li>
+                                    <li>7 año: $1,493m<sup>2</sup></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <!-- Pie de página -->
-    <footer>
-        <p>Todos los derechos reservados &copy; Fraccionamiento Campestre 2023</p>
+    <!-- Plan Section -->
+    <section id="plan" class="plan-section">
+        <div class="container">
+            <h2 class="text-center">Plano de Distribución</h2>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="{{ asset('images/planos/Verde_Ladera.svg') }}" alt="Plano de Distribución" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <h2 class="text-center">Contáctanos</h2>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Nombre">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+
+                            <textarea class="form-control" rows="5" placeholder="Mensaje"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Enviar Mensaje</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer Section -->
+    <footer class="footer-section">
+        <div class="container text-center">
+            <p>&copy; 2023 {{ $development->name }} . Todos los derechos reservados.</p>
+        </div>
     </footer>
+
+    <script src="https://kit.fontawesome.com/your-font-awesome-kit.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
