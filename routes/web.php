ce4b5eef196964d0c6a01ba940f6b9765c74109c
Sergio Lopez <sergio0695@gmail.com>
2023-07-23 23:00:00 -0600

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\SalesPerson\LandingPageController;
 use App\Http\Controllers\SubdomainController;
 use App\Http\Controllers\WaController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,16 +19,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::domain('{subdomain}.fraccionamientoscolima.com')->group(function () {
-    Route::get('/', [SubdomainController::class,'index']);
+    Route::get('/', [SubdomainController::class, 'index']);
 });
 Route::domain('{subdomain}.{subdomain2}.fraccionamientoscolima.com')->group(function () {
-    Route::get('/dashboard', [SubdomainController::class,'dashboard']);
+    Route::get('/dashboard', [SubdomainController::class, 'dashboard']);
 });
 
-Route::get('/webhook', [WaController::class,'webhook']);
-Route::post('/webhook', [WaController::class,'recibe']);
+Route::get('/webhook', [WaController::class, 'webhook']);
+Route::post('/webhook', [WaController::class, 'recibe']);
 Route::get('/privacy_policy', [EnterpriseController::class, 'privacyPolicy']);
-Route::get('syncSubdomains', [SubdomainController::class,'syncSubdomains']);
+Route::get('/syncSubdomains', [SubdomainController::class, 'syncSubdomains']);
 
 Route::get('/', function(){
     return view('landing_page.speedEstate');
