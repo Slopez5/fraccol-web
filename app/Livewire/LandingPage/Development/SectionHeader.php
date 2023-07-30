2b9d9d18e16c08274f500b23f5b1e353482a455b
Sergio Lopez <sergio0695@gmail.com>
2023-07-30 14:11:12 -0600

    Actualizacion de diseno para Header

--- app/Livewire/LandingPage/Development/SectionHeader.php
+++ app/Livewire/LandingPage/Development/SectionHeader.php
@@ -9,11 +9,26 @@ class SectionHeader extends Component
 {
     public $development_name = 'Fraccionamiento Verde Ladera';
     public $slogan = 'Bienvenido al lugar perfecto para vivir rodeado de naturaleza.';
-    public $background = '';
+    public $images = [];
+    public $width = 0;
 
 
     public function mount() {
-        $this->background = Storage::url('developments/Palmares/Foto_5.jpeg');
+        $this->images = [
+            [
+                "id" => 1,
+                "image" => Storage::url('developments/Palmares/Foto_1.jpeg')
+            ],
+            [
+                "id" => 2,
+                "image" => Storage::url('developments/Palmares/Foto_2.jpeg')
+            ],
+            [
+                "id" => 3,
+                "image" => Storage::url('developments/Palmares/Foto_3.jpeg')
+            ]
+        ];
+        $this->width = count($this->images)*100;
     }
     public function render()
     {
