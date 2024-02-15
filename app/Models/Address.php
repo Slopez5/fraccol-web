6a36fa152369c5a0261d8564de9b620db89981cf
Sergio Lopez <sergio0695@gmail.com>
2024-02-15 15:54:36 -0600

    se agrego neighborhood en el modelo

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -13,7 +13,7 @@ class Address extends Model
 
     protected $table = 'addresses';
 
-    protected $fillable = ['street','country_id','state_id','city_id','zip_code'];
+    protected $fillable = ['street','neighborhood','country_id','state_id','city_id','zip_code'];
 
     public function branch(): HasOne {
         return $this->hasOne(RealEstateBranch::class);
